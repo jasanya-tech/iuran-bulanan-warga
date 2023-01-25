c0f3bb5feb0a4debfbe004e5aa482f01b5bcff6b
Laila Alfi Syah <lailaalfisyah@gmail.com>
2023-01-25 14:41:48 +0700

    Add pagination for houses

--- src/main/java/com/iuran_bulanan_warga/Controllers/HouseFeaturesController.java
+++ src/main/java/com/iuran_bulanan_warga/Controllers/HouseFeaturesController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,7 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.websocket.server.PathParam;
 
 @RestController
-@RequestMapping("api/houses/features")
+@RequestMapping("api/houses/views")
 @CrossOrigin(origins = "*", maxAge = 3600)
 @Tag(name = "House Features", description = "Additional features for managing the UI of houses data")
 public class HouseFeaturesController {
@@ -22,8 +23,16 @@ public class HouseFeaturesController {
   HouseFeaturesService houseFeaturesService;
 
   @GetMapping("/{id}")
-    @Operation(summary = "Show House Data", description = "Showing the specification of house")
-    public ResponseEntity<?> showHouse(@PathParam("id") Integer houseId) {
-      return houseFeaturesService.showHouseData(houseId);
+  @Operation(summary = "Show One House Data", description = "Showing owner name, city name, and province name of house")
+  public ResponseEntity<?> showHouseByID(@PathParam("id") Integer houseId) {
+    return houseFeaturesService.showHouseData(houseId);
+  }
+
+  @GetMapping("/page")
+  @Operation(summary = "Show Houses Data", description = "Showing the data of houses with pagination")
+  public ResponseEntity<?> showHousesWithPage(
+    @RequestParam(defaultValue = "0") int page,
+    @RequestParam(defaultValue = "3") int size) {
+      return houseFeaturesService.showHousesWithPage(page, size);
     }
 }
--- src/main/java/com/iuran_bulanan_warga/Services/HouseFeaturesService.java
+++ src/main/java/com/iuran_bulanan_warga/Services/HouseFeaturesService.java
@@ -1,5 +1,9 @@
 package com.iuran_bulanan_warga.Services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import com.iuran_bulanan_warga.Helpers.DTO.Responses.HouseResponse;
@@ -8,6 +12,10 @@ import com.iuran_bulanan_warga.Models.Entities.Houses;
 import com.iuran_bulanan_warga.Models.Repositories.HouseRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -25,8 +33,30 @@ public class HouseFeaturesService {
       houseResponse.setCityName(house.get().getCity().getCityName());
       houseResponse.setProvinceName(house.get().getCity().getProvince().getProvinceName());
       return ResponseEntity.ok().body(houseResponse);
-    } catch (Exception e) {
+    }
+    catch (Exception e) {
       return ResponseEntity.ok().body(new MessageResponse(e.getMessage()));
+    }
+  }
+
+  public ResponseEntity<?> showHousesWithPage(int page, int size) {
+    try {
+      List<Houses> houses = new ArrayList<Houses>();
+
+      Pageable paging = PageRequest.of(page, size);
+      Page<Houses> pageHouses;
+      pageHouses = houseRepository.findAll(paging);
+      houses = pageHouses.getContent();
+
+      Map<String, Object> res = new HashMap<>();
+      res.put("houses", houses);
+      res.put("currentPage", paging.getPageNumber());
+      res.put("totalItems", pageHouses.getTotalElements());
+      res.put("totalPage", pageHouses.getTotalPages());
+      return ResponseEntity.ok().body(res);
+    }
+    catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
